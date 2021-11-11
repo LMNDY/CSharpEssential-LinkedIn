b8e3f4e15a86c04fa76eb36b3710f8985bc2f2e1
L-Mundy <32870334+L-Mundy@users.noreply.github.com>
2021-11-11 21:21:03 +0000

    Add project files.

---
+++ CSharpEssential-LinkedIn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpEssential_LinkedIn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
